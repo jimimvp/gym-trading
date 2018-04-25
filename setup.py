e0b6603a1ff3649fbcddb72af67b8b07c1f2f160
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2018-04-25 12:54:56 +0200

    Added initial package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+   name='gym_trading',
+   version='0.1',
+   description='Trading environments for commodities and equities with historical data support',
+   author='Marin Vlastelica',
+   author_email=None,
+   packages=find_packages(),
+   install_requires=['gym', 'pandas', 'numpy'],
+)
